dc018827d84841fb20f735d3ba487af07ed0081a
gayanesimonyan <gayane.simonian.simoni@gmail.com>
2021-09-04 14:29:17 +0400

    task gayane

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-alert('asd')
+function isMagic(square) {
+    let rows = square.map((item, i, array) => {
+      return item.reduce((sum, el, j, subarray) => {
+        return sum += el;
+      }, 0)
+    })
+    let columns = square.map((item, i, array) => {
+      let sum = 0
+      item.forEach((el, j, subarray) => {
+        sum += array[j][i]
+      })
+      return sum
+    })
+  
+    let firstDiagonal = square.reduce((sum, item, i, array) => {
+      return sum += array[i][i]
+    }, 0)
+  
+    let secondDiagonal = square.reduce((sum, item, i, array) => {
+      return sum += array[i][array.length - 1 - i]
+    }, 0)
+  
+    let result = new Set(rows.concat(columns, firstDiagonal, secondDiagonal))
+  
+    return result.size === 1
+  }
+  
+  
+  
+  
+  console.log(isMagic([
+        [2, 7, 6],
+        [9, 5, 1],
+        [4, 3, 8]
+      ]))
